5875e83c7c0d0586a53ddd8141378d7add71e2ca
sorainnosia <sorainnosia@gmail.com>
2017-11-11 22:37:51 +0800

    HWZEraser
    Hardwarezone post and thread eraser

---
+++ Form1.Designer.cs
@@ -0,0 +1,155 @@
+﻿namespace HWZEraser
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtUsername = new System.Windows.Forms.TextBox();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.rtbErase = new System.Windows.Forms.RichTextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.button1.Location = new System.Drawing.Point(320, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(78, 94);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Erase && Wait";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(99, 28);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Username";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 65);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(93, 28);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Password";
+            // 
+            // txtUsername
+            // 
+            this.txtUsername.Location = new System.Drawing.Point(146, 19);
+            this.txtUsername.Name = "txtUsername";
+            this.txtUsername.Size = new System.Drawing.Size(168, 34);
+            this.txtUsername.TabIndex = 0;
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(146, 59);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.PasswordChar = '*';
+            this.txtPassword.Size = new System.Drawing.Size(168, 34);
+            this.txtPassword.TabIndex = 1;
+            // 
+            // rtbErase
+            // 
+            this.rtbErase.Location = new System.Drawing.Point(17, 112);
+            this.rtbErase.Name = "rtbErase";
+            this.rtbErase.Size = new System.Drawing.Size(381, 138);
+            this.rtbErase.TabIndex = 3;
+            this.rtbErase.Text = "";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("STLiti", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label3.Location = new System.Drawing.Point(17, 255);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(94, 20);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "By : nubitol";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("STLiti", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label4.Location = new System.Drawing.Point(178, 255);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(220, 20);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Powered by ScrapperMin.com";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 28F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.ClientSize = new System.Drawing.Size(410, 284);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.rtbErase);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.txtUsername);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "HWZ Eraser";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtUsername;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.RichTextBox rtbErase;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using ScrapperMinDLL;
+
+namespace HWZEraser
+{
+    public partial class Form1 : Form
+    {
+        public Thread RunningThread = null;
+        public string Scripts = @"ERRORRESUME('1');
+REM('TITLE : HWZ Auto Bump');
+REM('WIDTH : 400');
+REM('PARAM 0 : HWZ Username : 20');
+REM('PARAM 1 : HWZ Password : 20');
+SET('User', '{PARAM0}');
+SET('Pass', '{PARAM1}');
+SET('PG', WC_GetPage('https://secureforums.hardwarezone.com.sg/logon.php', ''));
+SET('PS', WC_GetPostStringRaw('https://secureforums.hardwarezone.com.sg/logon.php', GET('PG'), '1', FORMAT('vb_login_username={0}&vb_login_md5password_utf={1}&vb_login_md5password={2}', GET('User'), SO_GetMd5Hash(GET('Pass')),SO_GetMd5Hash(GET('Pass'))), ''));
+SET('PG2', WC_PostPageComplete(WC_GetLastActionUrl(), GET('PS'), 'http://forums.hardwarezone.com.sg'));
+IF(GET('PG2'), 'CONTAINS', 'Thank you for logging', 
+    LOG('Successfully Login');
+    SET('PG2', WC_MethodPage('http://forums.hardwarezone.com.sg/eat-drink-man-woman-16/', 'GET', '', ''));
+    SET('UID', SO_TagMatch(GET('PG2'), ',""user"":{""id"":""', '""'));
+    SET('UR', JOIN('', 'http://forums.hardwarezone.com.sg/search.php?do=finduser&u=', GET('UID')));
+    SET('PG0', WC_MethodPageStop(GET('UR'), 'GET', '', JOIN('', 'Upgrade-Insecure-Requests=1&referer=', GET('UR'))));
+    SET('SID', SO_TagMatch(GET('PG0'), '/search.php?searchid=', '&'));
+    SET('URL', JOIN('', 'http://forums.hardwarezone.com.sg/search.php?searchid=', GET('SID')));
+    SET('URLX', JOIN('', GET('URL')));
+    SET('PG3', WC_MethodPage(GET('URLX'), 'GET', '', ''));
+    SET('SID', SO_TagMatch(GET('PG3'), 'searchid=', '&'));
+    SET('URL', JOIN('', 'http://forums.hardwarezone.com.sg/search.php?searchid=', GET('SID')));
+    SET('I', '1');
+    WHILE('1', '=', '1',
+       SET('URLX', JOIN('', GET('URL'), '&pp=25&page=', GET('I')));
+       SET('PG3', WC_MethodPage(GET('URLX'), 'GET', '', ''));
+       IF(GET('PG3'), 'CONTAINS', 'requires that you wait', LOG('WAIT'); SLEEP('15'); CONTINUE(), PASS());       
+       SET('STRS', SO_TagMatch(GET('PG3'), '#post', '""'));
+       SET('HAS', '0');
+       IF(COUNT(GET('STRS')), '<=', '0', EXIT('Finish'),
+          FOR('STR', GET('STRS'),
+             SET('ISDONE', SO_TagMatch(GET('PG3'), JOIN('', '#post', GET('STR'), '"">'), '<'));
+             IF(GET('ISDONE'), 'CONTAINS', 'edited', LOG('Skip');CONTINUE(), PASS());
+             SET('URL2', JOIN('', 'http://forums.hardwarezone.com.sg/editpost.php?do=editpost&p=', GET('STR')));
+             SET('PG4', WC_MethodPage(GET('URL2'), 'GET', '', ''));
+             IF(GET('PG4'), 'CONTAINS', 'This thread is closed', LOG('Closed'); CONTINUE(), PASS());
+
+             SET('TOKEN', SO_TagMatchSingle(GET('PG4'), 'name=""securitytoken""', 'value=""', '""'));
+             SET('URL3', JOIN('', 'http://forums.hardwarezone.com.sg/editpost.php?do=deletepost&p=', GET('STR')));
+             SET('PS2', JOIN('', 's=&securitytoken=', GET('TOKEN'), '&p=', GET('STR'), '&url=index.php&do=deletepost&deletepost=delete&reason=it+is+best+not+to+say+too+much'));
+             SET('PG5', WC_MethodPage(GET('URL3'), 'POST', GET('PS2'), ''));
+             IF(GET('PG5'), 'CONTAINS', 'you do not have permission to access', 
+                SET('URL2', JOIN('', 'http://forums.hardwarezone.com.sg/editpost.php?do=editpost&p=', GET('STR')));
+                SET('PG4', WC_MethodPage(GET('URL2'), 'GET', '', ''));
+                SET('TOKEN', SO_TagMatchSingle(GET('PG4'), 'name=""securitytoken""', 'value=""', '""'));
+                SET('URL3', JOIN('', 'http://forums.hardwarezone.com.sg/editpost.php?do=updatepost&p=', GET('STR')));
+                SET('PS2', JOIN('', 's=&securitytoken=', GET('TOKEN'), '&p=', GET('STR'), 'reason=&title=edited&message=edited+for+good&wysiwyg=0&iconid=0&do=updatepost&posthash=&poststarttime=&sbutton=Save+Changes&parseurl=1&emailupdate=9999'));
+                SET('PG5', WC_MethodPage(GET('URL3'), 'POST', GET('PS2'), ''));
+                IF(GET('STR'), '=', '105511711', EXIT(GET('PG5')), PASS());
+                IF(GET('PG5'), 'CONTAINS', 'you do not have permission to access', 
+                   LOG('Fail')
+                , 
+                   IF(GET('PG5'), 'CONTAINS', 'This thread is closed', 
+                      LOG('Unable to Delete Post due to Thread Closed');
+                   , SET('HAS', '1');LOG('1 Post Deleted'));
+                );
+             , 
+                IF(GET('PG5'), 'CONTAINS', 'This thread is closed', 
+                   LOG('Unable to Delete Post due to Thread Closed');
+                , SET('HAS', '1');LOG('1 Post Deleted'));
+             );
+          );
+       );
+       IF(GET('HAS'), '=', '0', SET('I', MI_Add(GET('I'), '1')), PASS());
+    );
+, EXIT('Invalid Login'));";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (button1.Text == "Stop")
+            {
+                button1.Text = "Erase && Wait";
+                Stop();
+            }
+            else
+            {
+                if (string.IsNullOrEmpty(txtUsername.Text) || string.IsNullOrEmpty(txtPassword.Text))
+                {
+                    MessageBox.Show("Please input username and password");
+                    return;
+                }
+                button1.Text = "Stop";
+                Start();
+            }
+        }
+
+        private List<string> DataList = new List<string>();
+        private void Start()
+        {
+            DataList.Clear();
+            DataList.Add(txtUsername.Text);
+            DataList.Add(txtPassword.Text);
+
+            ThreadStart ts = new ThreadStart(RunThread);
+            RunningThread = new Thread(ts);
+            RunningThread.Start();
+        }
+
+        private void RunThread()
+        {
+            ScrapperMin sm = ScrapperMin.InitWC();
+            sm.Log = new Action<string>(AddLog);
+            string[] result = sm.Multiple(Scripts, DataList);
+        }
+
+        private void AddLog(string str)
+        {
+            if (InvokeRequired)
+            {
+                Invoke(new Action<string>(AddLog), str);
+                return;
+            }
+            rtbErase.AppendText(str + "\r\n");
+        }
+
+        private void Stop()
+        {
+            if (RunningThread != null)
+            {
+                try
+                {
+                    RunningThread.Abort();
+                    RunningThread = null;
+                }
+                catch { }
+            }
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            Stop();
+            Environment.Exit(0);
+        }
+    }
+}
